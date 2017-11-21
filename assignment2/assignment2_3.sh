73060468145c710c1e85f61e32cc3fd1f47a0caf
vagrant <vagrant@gateway.localdomain>
2017-11-21 16:31:48 +0000

    minor fix

--- assignment2/assignment2_3.sh
+++ assignment2/assignment2_3.sh
@@ -8,12 +8,15 @@ echo $BUFFETT
 
 # First step: replace first "snow" with "foot" 
 BUFFETT1="${BUFFETT/snow/foot}"
+echo $BUFFETT1
 
 # Second step: delete remaining "snow"
 BUFFETT2="${BUFFETT1/snow/}"
+echo $BUFFETT2
 
 # Third step: replace "finding" with "getting"
 BUFFETT3="${BUFFETT2/finding/getting}"
+echo $BUFFETT3
 
 # Fourth step: remove everything after "wet"
 BUFFETT4="${BUFFETT3%* and*}"
