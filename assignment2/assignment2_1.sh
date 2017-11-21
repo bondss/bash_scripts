0ee5449f3bca1a9d32923904fa0bf050797e3af6
my <my@localhost.localdomain>
2017-11-21 12:40:46 +0200

    add indents

--- assignment2/assignment2_1.sh
+++ assignment2/assignment2_1.sh
@@ -5,16 +5,12 @@ echo "Input your income (only number, no currency!): "
 read income
 
 #Calculating and displaying taxes for users depending on their income
-if [ $income -lt 5000 ]
-then
-echo "No tax for you!"
-
-elif [[ $income -ge 5000 && $income -le 30000 ]]
-then
-let "tax=$income/10"; echo "Your tax is: $tax"
-
+if [ $income -lt 5000 ]; then
+    echo "No tax for you!"
+elif [[ $income -ge 5000 && $income -le 30000 ]]; then
+    let "tax=$income/10"; echo "Your tax is: $tax"
 else 
-let "tax=$income*2/10"; echo "Your tax is: $tax"
+   let "tax=$income*2/10"; echo "Your tax is: $tax"
 fi
 
 exit 0
